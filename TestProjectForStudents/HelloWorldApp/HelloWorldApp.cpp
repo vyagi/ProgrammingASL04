e190b18fce65a12b56c32f0b9efad2da42896dd1
Marcin Jagieła <marcin.jagiela@hotmail.com>
2021-03-26 17:40:01 +0100

    Improved a message

--- TestProjectForStudents/HelloWorldApp/HelloWorldApp.cpp
+++ TestProjectForStudents/HelloWorldApp/HelloWorldApp.cpp
@@ -5,7 +5,7 @@
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    std::cout << "Hello World! We love programming in C++ (or maybe not)";
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
