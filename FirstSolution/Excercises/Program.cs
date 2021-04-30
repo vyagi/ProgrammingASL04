db403db912aa18ca98a02a8d5a8e2c56e694275e
Marcin Jagieła <marcin.jagiela@hotmail.com>
2021-04-30 17:02:27 +0200

    Task 3 completed

--- FirstSolution/Excercises/Program.cs
+++ FirstSolution/Excercises/Program.cs
@@ -10,8 +10,16 @@ namespace Excercises
             // Console.WriteLine("Hello");
 
             //Task 2
+            // var name = "Marcin";
+            // Console.WriteLine(name);
+
+            //Task 3
             var name = "Marcin";
-            Console.WriteLine(name);
+            Console.WriteLine("Hello {0}", name); //This is old school
+            Console.WriteLine("Hello " + name); //Don't use it
+            Console.WriteLine(string.Format("Hello {0}", name)); // This is crappy old school
+            Console.WriteLine($"Hello {name}");  //Use this 
+
         }
     }
 }
