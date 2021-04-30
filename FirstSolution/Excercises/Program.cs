ac5b52f114a603b94dc0999aa22deca87c1a9c8e
Marcin Jagieła <marcin.jagiela@hotmail.com>
2021-04-30 16:57:16 +0200

    Task 1 completed

---
+++ FirstSolution/Excercises/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Excercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Task 1
+            // Console.WriteLine("Hello");
+
+
+        }
+    }
+}
