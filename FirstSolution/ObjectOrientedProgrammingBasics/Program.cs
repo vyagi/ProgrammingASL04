8f133563c54a25f09544a8d03a04a45dd3b47827
Marcin Jagieła <marcin.jagiela@hotmail.com>
2021-05-07 17:39:21 +0200

    Point class done

--- FirstSolution/ObjectOrientedProgrammingBasics/Program.cs
+++ FirstSolution/ObjectOrientedProgrammingBasics/Program.cs
@@ -4,6 +4,8 @@ namespace ObjectOrientedProgrammingBasics
 {
     class Point
     {
+        public static int Dummy;
+        
         private double _x;
         private double _y;
 
@@ -22,8 +24,37 @@ namespace ObjectOrientedProgrammingBasics
 
         public double Distance(Point another) => 
             Math.Sqrt(Math.Pow(X - another.X, 2) + Math.Pow(Y - another.Y, 2));
+
+        public static double Distance(Point first, Point second) => 
+            first.Distance(second);
+
+        public Point Reflect(ReflectionType reflectionType)
+        {
+            switch (reflectionType)
+            {
+                case ReflectionType.X:
+                    return new Point(X, -Y);
+                case ReflectionType.Y:
+                    return new Point(-X, Y);
+                case ReflectionType.Origin:
+                    return new Point(-X, -Y);
+                default:
+                    throw new Exception("Invalid reflection type");
+            }
+        }
+
+        public enum ReflectionType
+        {
+            X,
+            Y,
+            Origin
+        }
+
+        public override string ToString() => $"({X},{Y})";
     }
-    
+
+
+
     class Program
     {
         static void Main(string[] args)
@@ -39,6 +70,14 @@ namespace ObjectOrientedProgrammingBasics
             Console.WriteLine(p2.Distance());
 
             Console.WriteLine(p1.Distance(p2));
+
+            Console.WriteLine(Point.Distance(p1, p2));
+
+            var reflectedP1 = p1.Reflect(Point.ReflectionType.X);
+            Console.WriteLine(reflectedP1.X);
+            Console.WriteLine(reflectedP1.Y);
+
+            Console.WriteLine(p1);
         }
     }
 }
