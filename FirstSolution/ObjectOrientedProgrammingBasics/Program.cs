45eccee709d077f4a088a63d00c34f1c03433ce1
Marcin Jagieła <marcin.jagiela@hotmail.com>
2021-05-07 16:46:43 +0200

    Properties done

--- FirstSolution/ObjectOrientedProgrammingBasics/Program.cs
+++ FirstSolution/ObjectOrientedProgrammingBasics/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Net.Http.Headers;
 
 namespace ObjectOrientedProgrammingBasics
 {
@@ -8,46 +9,73 @@ namespace ObjectOrientedProgrammingBasics
         private int _weight;
         private string _name;
 
+        public int Height => _height;
+
+        public int Weight
+        {
+            get => _weight;
+            set
+            {
+                if (value <= 0)
+                    throw new Exception("Weight should be positive");
+                
+                _height = value;
+            }
+        }
+
+        public string Name
+        {
+            get => _name;
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                    throw new Exception("Name cannot be null"); 
+                
+                _name = value;
+            }
+        }
+
+
 
         //Java way
-        public int GetHeight()
-        {
-            return _height;
-        }
-
-        public void SetHeight(int newHeight)
-        {
-            if (newHeight <= 0)
-                throw new Exception("Height should be positive");
-
-            _height = newHeight;
-        }
-
-        public int GetWeight()
-        {
-            return _weight;
-        }
-
-        public void SetWeight(int newWeight)
-        {
-            if (newWeight <= 0)
-                throw new Exception("Weight should be positive");
-
-            _height = newWeight;
-        }
-
-        public string GetName()
-        {
-            return _name;
-        }
-
-        public void SetName(string newName)
-        {
-            if (string.IsNullOrWhiteSpace(newName))
-                throw new Exception("Name cannot be null");
-
-            _name = newName;
-        }
+        // public int GetHeight()
+        // {
+        //     return _height;
+        // }
+        //
+        // public void SetHeight(int newHeight)
+        // {
+        //     if (newHeight <= 0)
+        //         throw new Exception("Height should be positive");
+        //
+        //     _height = newHeight;
+        // }
+        //
+        // public int GetWeight()
+        // {
+        //     return _weight;
+        // }
+        //
+        // public void SetWeight(int newWeight)
+        // {
+        //     if (newWeight <= 0)
+        //         throw new Exception("Weight should be positive");
+        //
+        //     _height = newWeight;
+        // }
+        //
+        // public string GetName()
+        // {
+        //     return _name;
+        // }
+        //
+        // public void SetName(string newName)
+        // {
+        //     if (string.IsNullOrWhiteSpace(newName))
+        //         throw new Exception("Name cannot be null");
+        //
+        //     _name = newName;
+        // }
 
         public Person(string name, int weight, int height)
         {
@@ -78,10 +106,15 @@ namespace ObjectOrientedProgrammingBasics
         {
             var you = new Person("Marcin", 100, 190);
 
-            Console.WriteLine(you.GetName());
-            you.SetName(null);
-            Console.WriteLine(you.GetName());
-            
+            // Console.WriteLine(you.GetName());
+            // you.SetName(null);
+            // Console.WriteLine(you.GetName());
+
+            Console.WriteLine(you.Name);
+            you.Name = "Robert";
+            you.Weight = 60;
+            Console.WriteLine(you.Height);
+            Console.WriteLine(you.Name);
 
             // you.Name = "Marcin";
             // you.Height = 180;
