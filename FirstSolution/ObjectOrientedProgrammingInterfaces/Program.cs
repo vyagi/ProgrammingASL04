5333125b2b3dc17ff31bd04ad7143b8e23437210
Marcin Jagieła <marcin.jagiela@hotmail.com>
2021-06-04 17:04:24 +0200

    Geometry project added

---
+++ FirstSolution/ObjectOrientedProgrammingInterfaces/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ObjectOrientedProgrammingInterfaces
+{
+    public interface IShape //old school interfaces do not allow for any implementation and the do not allow for public keyword
+    //just return type, name and list of parameters.
+    {
+        double Area();
+        
+    }
+
+    public interface ICreature
+    {
+        void Breathe();
+    }
+
+    public class LivingSquare : IShape, ICreature
+    {
+        public double Area()
+        {
+            return 10;
+        }
+
+        public void Breathe()
+        {
+
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
